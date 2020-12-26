55451aa8135214ab0c1c603e9c6f0d9f8f09ee2b
majklfly <majklfly@gmail.com>
2020-12-26 20:34:08 +0000

    first occuring problem

--- codecademyExamples.js
+++ codecademyExamples.js
@@ -1,3 +1,51 @@
+// FIRST CHARACTER PROBLEM
+const first_recurring = (given_string) => {
+    const counts = {};
+    let theLetter = "not found";
+    const string_splitted = given_string.split("");
+    for (let i = 0; i < string_splitted.length; i++) {
+        if (string_splitted[i] in counts) {
+            theLetter = string_splitted[i];
+            break;
+        }
+        counts[string_splitted[i]] = true;
+    }
+    return theLetter;
+};
+
+console.log(first_recurring("Milchal"));
+
+// DYNAMIC KNAPSACK
+// const dynamicKnapsack = function(weightCap, weights, values) {
+//     const numItem = weights.length;
+//     const matrix = new Array(numItem);
+
+//     for (let index = 0; index <= numItem; index++) {
+//         matrix[index] = new Array(weightCap + 1);
+//         for (let weight = 0; weight <= weightCap; weight++) {
+//             // Write your code here:
+//             if (index === 0 || weight === 0) {
+//                 matrix[index][weight] = 0;
+//             } else if (weights[index - 1] <= weight) {
+//                 const includeItem =
+//                     values[index - 1] + matrix[index - 1][weight - weights[index - 1]];
+//                 const excludeItem = matrix[index - 1][weight];
+//                 matrix[index][weight] = Math.max(includeItem, excludeItem);
+//             } else {
+//                 matrix[index][weight] = matrix[index - 1][weight];
+//             }
+//         }
+//     }
+//     return matrix[numItem][weightCap];
+// };
+
+// // Use this to test your function:
+// const weightCap = 50;
+// const weights = [31, 10, 20, 19, 4, 3, 6];
+// const val = [70, 20, 39, 37, 7, 5, 10];
+// console.log(dynamicKnapsack(weightCap, weights, val));
+//
+
 // FIBONACCI WITH MEMOAZING
 
 // const memo = {};
