988e312b20ca4f2ac149ad3e5a98be3357cb1e81
majklfly <majklfly@gmail.com>
2020-12-27 22:33:25 +0000

    added sum problems;

--- DynamicProgrammingFCC.js
+++ DynamicProgrammingFCC.js
@@ -1,3 +1,50 @@
+// HOWSUM PROBLEM
+
+const howSum = (targetSum, numbers) => {
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    for (let num of numbers) {
+        console.log("targetSum", targetSum);
+        console.log("num", num);
+        const remainder = targetSum - num;
+        console.log("remainer", remainder);
+        const remainderResult = howSum(remainder, numbers);
+        if (remainderResult !== null) {
+            return [...remainderResult, num];
+        }
+    }
+    return null;
+};
+// console.log(howSum(7, [2, 3]));
+console.log(howSum(7, [5, 3, 4, 7]));
+// console.log(howSum(7, [2, 4]));
+// console.log(howSum(8, [2, 3, 5]));
+// console.log(howSum(300, [7, 14]));
+
+// CANSUM PROBLEM
+// const canSum = (targetSum, numbers, memo = {}) => {
+//     if (targetSum in memo) return memo[targetSum];
+//     if (targetSum === 0) return true;
+//     if (targetSum < 0) return false;
+
+//     for (let num of numbers) {
+//         const remainder = targetSum - num;
+//         if (canSum(remainder, numbers, memo) === true) {
+//             memo[targetSum] = true;
+//             return true;
+//         }
+//     }
+//     memo[targetSum] = false;
+//     return false;
+// };
+
+// console.log(canSum(7, [2, 3]));
+// console.log(canSum(7, [5, 3, 4, 7]));
+// console.log(canSum(7, [2, 4]));
+// console.log(canSum(8, [2, 3, 5]));
+// console.log(canSum(300, [7, 14]));
+
 // GRID TRAVELLER
 // const gridTraveler = (m, n, memo = {}) => {
 //     const key = m + "," + n;
---
+++ LinkedInAssessment.js
@@ -0,0 +1,36 @@
+// sum(10, 20);
+// diff(10, 20);
+
+// function sum(x, y) {
+//     return x + y;
+// }
+// let diff = function(x, y) {
+//     return x - y;
+// };
+
+// class obj {
+//     get Y() {
+//         return 42;
+//     }
+// }
+// const x = new obj();
+// console.log(x.Y);
+
+// var a = ["dog", "cat", "hen"];
+// a[100] = "fox";
+// console.log(a.length);
+
+// var Storm = function() {};
+// Storm.prototype.precip = "rain";
+// var WinterStorm = function() {};
+// WinterStorm.prototype = new Storm();
+// WinterStorm.prototype.precip = "snow";
+// var bob = new WinterStorm();
+// console.log(bob.precip);
+
+// for (var i = 1; i <= 4; i++) {
+//     var anon = function(j) {
+//         console.log("I am anonymous", j);
+//     };
+//     anon(i);
+// }
