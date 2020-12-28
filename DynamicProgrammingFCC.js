2ee81c09dd3793e016f254b4519ee68d7bc114f8
majklfly <majklfly@gmail.com>
2020-12-28 20:40:48 +0000

    all construct problem tabulation

--- DynamicProgrammingFCC.js
+++ DynamicProgrammingFCC.js
@@ -1,26 +1,106 @@
-// BESTSUM PROBLEM
+// ALL CONSTRUCT PROBLEM
 
-const bestSum = (targetSum, numbers) => {
-    const table = Array(targetSum + 1).fill(null);
-    table[0] = [];
-    for (let i = 0; i <= targetSum; i++) {
-        if (table[i] !== null) {
-            for (let num of numbers) {
-                const combination = [...table[i], num];
-                if (!table[i + num] || table[i + num].length > combination.length) {
-                    table[i + num] = combination;
-                }
+const allConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1)
+        .fill()
+        .map(() => []);
+    table[0] = [
+        []
+    ];
+    for (let i = 0; i <= target.length; i++) {
+        for (let word of wordBank) {
+            if (target.slice(i, i + word.length) === word) {
+                const newCombination = table[i].map((subArray) => [...subArray, word]);
+                table[i + word.length].push(...newCombination);
             }
         }
     }
 
-    return table[targetSum];
+    return table[target.length];
 };
 
-console.log(bestSum(7, [5, 3, 4, 7]));
-console.log(bestSum(8, [2, 3, 5]));
-console.log(bestSum(8, [1, 4, 5]));
-console.log(bestSum(100, [1, 2, 5, 25]));
+console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+    allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+    allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+);
+
+// COUNT CONSTRUCT PROBLEM
+
+// const countConstruct = (target, wordBank) => {
+//     const table = Array(target.length + 1).fill(0);
+//     table[0] = 1;
+//     for (let i = 0; i <= target.length; i++) {
+//         for (let word of wordBank) {
+//             if (target.slice(i, i + word.length) === word) {
+//                 table[i + word.length] += table[i];
+//             }
+//         }
+//     }
+
+//     return table[target.length];
+// };
+
+// console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+// console.log(
+//     countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+// );
+// console.log(
+//     countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+// );
+
+// CAN CONSTRUCT PROBLEM
+
+// const canConstruct = (target, wordBank) => {
+//     const table = Array(target.length + 1).fill(false);
+//     table[0] = true;
+
+//     for (let i = 0; i < target.length; i++) {
+//         if (table[i] === true) {
+//             for (let word of wordBank) {
+//                 if (target.slice(i, i + word.length) === word) {
+//                     table[i + word.length] = true;
+//                 }
+//             }
+//         }
+//     }
+
+//     return table[target.length];
+// };
+
+// console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+// console.log(
+//     canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+// );
+// console.log(
+//     canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+// );
+
+// BESTSUM PROBLEM
+
+// const bestSum = (targetSum, numbers) => {
+//     const table = Array(targetSum + 1).fill(null);
+//     table[0] = [];
+//     for (let i = 0; i <= targetSum; i++) {
+//         if (table[i] !== null) {
+//             for (let num of numbers) {
+//                 const combination = [...table[i], num];
+//                 if (!table[i + num] || table[i + num].length > combination.length) {
+//                     table[i + num] = combination;
+//                 }
+//             }
+//         }
+//     }
+
+//     return table[targetSum];
+// };
+
+// console.log(bestSum(7, [5, 3, 4, 7]));
+// console.log(bestSum(8, [2, 3, 5]));
+// console.log(bestSum(8, [1, 4, 5]));
+// console.log(bestSum(100, [1, 2, 5, 25]));
 
 // HOW SUM TABULATION
 
