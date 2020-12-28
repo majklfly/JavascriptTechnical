5b30bd782d3fe2d25ab65bb3f0c076dab804c502
majklfly <majklfly@gmail.com>
2020-12-28 18:22:15 +0000

    how sum tabulation problem

--- DynamicProgrammingFCC.js
+++ DynamicProgrammingFCC.js
@@ -1,23 +1,46 @@
+// HOW SUM TABULATION
+
+// const howSum = (targetSum, numbers) => {
+//     const table = Array(targetSum + 1).fill(null);
+//     table[0] = [];
+
+//     for (let i = 0; i <= targetSum; i++) {
+//         if (table[i] !== null) {
+//             for (let num of numbers) {
+//                 table[i + num] = [...table[i], num];
+//             }
+//         }
+//     }
+
+//     return table[targetSum];
+// };
+
+// console.log(howSum(7, [2, 3]));
+// console.log(howSum(7, [5, 3, 4, 7]));
+// console.log(howSum(7, [2, 4]));
+// console.log(howSum(8, [2, 3, 5]));
+// console.log(howSum(300, [7, 14]));
+
 // CAN SUM TABULATION SOLUTION
 
-const canSum = (targetSum, numbers) => {
-    const table = Array(targetSum + 1).fill(false);
-    table[0] = true;
-    for (let i = 0; i <= targetSum; i++) {
-        if (table[i] === true) {
-            for (let num of numbers) {
-                table[i + num] = true;
-            }
-        }
-    }
-    return table[targetSum];
-};
+// const canSum = (targetSum, numbers) => {
+//     const table = Array(targetSum + 1).fill(false);
+//     table[0] = true;
+//     for (let i = 0; i <= targetSum; i++) {
+//         if (table[i] === true) {
+//             for (let num of numbers) {
+//                 table[i + num] = true;
+//             }
+//         }
+//     }
+//     return table[targetSum];
+// };
 
-console.log(canSum(7, [2, 3]));
-console.log(canSum(7, [5, 3, 4, 7]));
-console.log(canSum(7, [2, 4]));
-console.log(canSum(8, [2, 3, 5]));
-console.log(canSum(300, [7, 14]));
+// console.log(canSum(7, [2, 3]));
+// console.log(canSum(7, [5, 3, 4, 7]));
+// console.log(canSum(7, [2, 4]));
+// console.log(canSum(8, [2, 3, 5]));
+// console.log(canSum(300, [7, 14]));
 
 // GRID TRAVELER TABULATION
 
