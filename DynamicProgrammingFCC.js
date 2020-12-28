b03164209b387fb66172abeaf3f00dd0330d94bf
majklfly <majklfly@gmail.com>
2020-12-28 18:05:00 +0000

    can sum tabulation

--- DynamicProgrammingFCC.js
+++ DynamicProgrammingFCC.js
@@ -1,5 +1,24 @@
 // CAN SUM TABULATION SOLUTION
 
+const canSum = (targetSum, numbers) => {
+    const table = Array(targetSum + 1).fill(false);
+    table[0] = true;
+    for (let i = 0; i <= targetSum; i++) {
+        if (table[i] === true) {
+            for (let num of numbers) {
+                table[i + num] = true;
+            }
+        }
+    }
+    return table[targetSum];
+};
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [5, 3, 4, 7]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3, 5]));
+console.log(canSum(300, [7, 14]));
+
 // GRID TRAVELER TABULATION
 
 // const gridTraveler = (m, n) => {
