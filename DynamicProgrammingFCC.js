8f9f7482774397aa88ea34cdb20a0ba324112138
majklfly <majklfly@gmail.com>
2020-12-28 11:14:21 +0000

    bestsum problem

--- DynamicProgrammingFCC.js
+++ DynamicProgrammingFCC.js
@@ -1,23 +1,60 @@
+// CAN CONSTRUCT STRING PROBLEM
+
+// BESTSUM PROBLEM
+
+// const bestSum = (targetSum, numbers, memo = {}) => {
+//     if (targetSum in memo) return memo[targetSum];
+//     if (targetSum === 0) return [];
+//     if (targetSum < 0) return null;
+
+//     let shortestCombination = null;
+
+//     for (let num of numbers) {
+//         const remainder = targetSum - num;
+//         const remainderCombination = bestSum(remainder, numbers, memo);
+//         if (remainderCombination !== null) {
+//             const combination = [...remainderCombination, num];
+//             if (
+//                 shortestCombination === null ||
+//                 combination.length < shortestCombination.length
+//             ) {
+//                 shortestCombination = combination;
+//             }
+//         }
+//     }
+
+//     memo[targetSum] = shortestCombination;
+//     return shortestCombination;
+// };
+
+// console.log(bestSum(7, [5, 3, 4, 7]));
+// console.log(bestSum(8, [2, 3, 5]));
+// console.log(bestSum(8, [1, 4, 5]));
+// console.log(bestSum(100, [1, 2, 5, 25]));
+
 // HOWSUM PROBLEM
 
-const howSum = (targetSum, numbers) => {
-    if (targetSum === 0) return [];
-    if (targetSum < 0) return null;
+// const howSum = (targetSum, numbers, memo = {}) => {
+//     if (targetSum in memo) return memo[targetSum];
+//     if (targetSum === 0) return [];
+//     if (targetSum < 0) return null;
 
-    for (let num of numbers) {
-        console.log("targetSum", targetSum);
-        console.log("num", num);
-        const remainder = targetSum - num;
-        console.log("remainer", remainder);
-        const remainderResult = howSum(remainder, numbers);
-        if (remainderResult !== null) {
-            return [...remainderResult, num];
-        }
-    }
-    return null;
-};
+//     for (let num of numbers) {
+//         console.log("targetSum", targetSum);
+//         console.log("num", num);
+//         const remainder = targetSum - num;
+//         console.log("remainer", remainder);
+//         const remainderResult = howSum(remainder, numbers, memo);
+//         if (remainderResult !== null) {
+//             memo[targetSum] = [...remainderResult, num];
+//             return memo[targetSum];
+//         }
+//     }
+//     memo[targetSum] = null;
+//     return null;
+// };
 // console.log(howSum(7, [2, 3]));
-console.log(howSum(7, [5, 3, 4, 7]));
+// console.log(howSum(7, [5, 3, 4, 7]));
 // console.log(howSum(7, [2, 4]));
 // console.log(howSum(8, [2, 3, 5]));
 // console.log(howSum(300, [7, 14]));
