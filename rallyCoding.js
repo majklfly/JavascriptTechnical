343660c33a99bace490931fa387735758e57ff1a
majklfly <majklfly@gmail.com>
2021-01-31 14:09:20 +0000

    sort array

--- rallyCoding.js
+++ rallyCoding.js
@@ -1,9 +1,10 @@
-const solve = (strArg, k) => {
-  const result = [];
-  for (let i = 0; i < k; i++) {
-    result.push(strArg);
+const solve = (intArray) => {
+  const sorted = intArray.sort((a, b) => a - b);
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] === sorted[i + 1]) {
+      return sorted[i];
+    }
   }
-  return result;
 };
 
-console.log(solve("abc", 5));
+console.log(solve([-6, 1, 5, -6, 0, -2, 3]));
