42f42a4db7349299117e6d32574b6346c8c9fc2d
majklfly <majklfly@gmail.com>
2021-02-01 19:11:00 +0000

    sorting

--- rallyCoding.js
+++ rallyCoding.js
@@ -1,18 +1,6 @@
-const solve = (arrOne, arrTwo) => {
-  const shorterArray = arrOne.length > arrTwo.length ? arrTwo : arrOne;
-  const longerArray = arrOne.length > arrTwo.length ? arrOne : arrTwo;
-  const result = [];
-  longerArray.forEach((number) => {
-    if (shorterArray.includes(number) && !result.includes(number)) {
-      result.push(number);
-    }
-  });
-  return result;
+const solve = (strArray) => {
+  const sorted = strArray.sort((a, b) => (a[1] > b[1] ? 1 : -1));
+  return sorted;
 };
 
-console.log(
-  solve(
-    [2, 3, 4, 7, 10, 5, 4, 2, 1000],
-    [3, 7, 10, 2, 2, 7, 7, 2, 1, 5, 4, 5, 1, 1, -1, 2]
-  )
-);
+console.log(solve(["@Winston", "@James", "@Albert", "@Sally"]));
