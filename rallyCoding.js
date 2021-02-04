571b261202021ce2452f8150fae2a856610811dd
majklfly <majklfly@gmail.com>
2021-02-04 13:12:05 +0000

    strings problem

--- rallyCoding.js
+++ rallyCoding.js
@@ -1,5 +1,15 @@
-const solve = (a, b, k) => {
-  return;
+const solve = (strArg) => {
+  const result = [];
+  for (let i = 0; i < strArg.length; i++) {
+    if (
+      strArg[i] + strArg[i + 1] + strArg[i + 2] ===
+      strArg[i + 2] + strArg[i + 1] + strArg[i]
+    ) {
+      result.push(strArg[i] + strArg[i + 1] + strArg[i + 2]);
+      i++;
+    }
+  }
+  return result;
 };
 
-console.log(solve(4, 16, 5));
+console.log(solve("rdrairekeaca"));
